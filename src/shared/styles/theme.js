ad1c0f4084674f3e3fadfabea8658ec4acd63a51
bongtta <kce0419@naver.com>
2025-05-18 00:58:56 +0900

    style: theme 추가

--- src/shared/styles/theme.js
+++ src/shared/styles/theme.js
@@ -1,14 +1,14 @@
-import { css } from "@emotion/react";
+import { css } from '@emotion/react';
 
 export const theme = {
   colors: {
     //gray
-    gray100: "#F8FAFB",
-    gray200: "#F2F4F5",
-    gray300: "#E5E7EA",
-    gray500: "#9CA2AE",
-    gray800: "#4B5563",
-    gray900: "#101827",
+    gray100: '#F8FAFB',
+    gray200: '#F2F4F5',
+    gray300: '#E5E7EA',
+    gray500: '#9CA2AE',
+    gray800: '#4B5563',
+    gray900: '#101827',
 
     //green
     green100: '#F8FAFB',
@@ -18,66 +18,78 @@ export const theme = {
 
   font: {
     // Title
-    'pre_title_semi_20': css`
+    pre_title_semi_20: css`
       font-weight: 600;
       font-size: 2rem;
       line-height: 140%;
     `,
-    'pre_title_semi_18': css`
+    pre_title_semi_18: css`
       font-weight: 600;
       font-size: 1.8rem;
       line-height: 140%;
     `,
 
     //Subtitle
-    'pre_subtitle_semi_18': css`
+    pre_subtitle_semi_18: css`
       font-weight: 600;
       font-size: 1.8rem;
       line-height: 140%;
     `,
 
-    'pre_subtitle_semi_16': css`
+    pre_subtitle_semi_16: css`
       font-weight: 600;
       font-size: 1.6rem;
       line-height: 140%;
     `,
 
     //Body
-    'pre_body_semi_14': css`
+    pre_body_med_16: css`
+      font-weight: 500;
+      font-size: 1.6rem;
+      line-height: 140%;
+    `,
+
+    pre_body_semi_14: css`
       font-weight: 600;
       font-size: 1.4rem;
       line-height: 140%;
     `,
 
-    'pre_body_reg_14': css`
+    pre_body_med_14: css`
+      font-weight: 500;
+      font-size: 1.4rem;
+      line-height: 140%;
+    `,
+
+    pre_body_reg_14: css`
       font-weight: 400;
       font-size: 1.4rem;
       line-height: 140%;
     `,
 
-    'pre_body_semi_12': css`
+    pre_body_semi_12: css`
       font-weight: 600;
       font-size: 1.2rem;
       line-height: 140%;
     `,
 
-    'pre_body_reg_12': css`
+    pre_body_reg_12: css`
       font-weight: 400;
       font-size: 1.2rem;
       line-height: 140%;
     `,
 
     //Caption
-    'pre_cap_semi_12': css`
+    pre_cap_semi_12: css`
       font-weight: 600;
       font-size: 1.2rem;
       line-height: 140%;
     `,
 
-    'pre_cap_reg_12': css`
+    pre_cap_reg_12: css`
       font-weight: 400;
       font-size: 1.2rem;
       line-height: 140%;
     `,
-  }
+  },
 };
