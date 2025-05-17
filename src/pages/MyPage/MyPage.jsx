45884f55e51894785a64aa64ae976716423e28b3
jstar000 <wltjd6300@gmail.com>
2025-05-17 23:31:17 +0000

    feat:

--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -1,12 +1,31 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import FilterButton from '@components/filter-button/FilterButton';
 import Card from '@components/Card';
 
 import styled from '@emotion/styled';
+import apiClient from '../../shared/apis/apiClient';
 
 export default function MyPage() {
-  const [selectedIdx, setSelectedIdx] = useState(0); // 인덱스로 상태 관리
-  const state = ['수락 대기', '수락 완료'];
+  const [selectedIdx, setSelectedIdx] = useState(0); 
+  const state = ["PENDING", "APPROVED"];
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchApply = async () => {
+      try {
+        const response = await apiClient.get(`/api/v1/apply/list`, {
+          params: {
+            state: state[selectedIdx], // 선택된 state 값을 params로 전달
+          },
+        });
+        setData(response.data.data.getPlaceResponseList);
+        console.log(response.data.data.getPlaceResponseList);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchApply();
+  }, [selectedIdx]); // selectedIdx가 변경될 때마다 실행
 
   const handleState = (idx) => {
     setSelectedIdx(idx);
@@ -16,15 +35,27 @@ export default function MyPage() {
     <PageWrapper>
       <StateWrapper>
         {state.map((label, idx) => (
-          <FilterButton key={idx} label={label} selected={selectedIdx === idx} onClick={() => handleState(idx)} />
+          <FilterButton 
+            key={idx} 
+            label={label} 
+            selected={selectedIdx === idx} 
+            onClick={() => handleState(idx)} 
+          />
         ))}
       </StateWrapper>
       <CardWrapper>
-        <Card /> <Card isCompleted={true} /> <Card />
+        {data.length > 0 ? (
+          data.map((datas, index) => (
+            <Card key={index} description={datas.description} farmer={datas.farmer} name={datas.name} photoUrl={datas.photoUrl} price={datas.price} />
+          ))
+        ) : (
+          <p>No data available.</p>
+        )}
       </CardWrapper>
     </PageWrapper>
   );
 }
+
 const PageWrapper = styled.div`
   padding: 0 2.8rem;
 `;
