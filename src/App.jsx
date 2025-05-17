22446b29b04f3f6c1429e7af0ec000c6c3ace669
jogpfls <joerin4177@gmail.com>
2025-05-17 23:24:26 +0900

    init: static 색상 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import styled from "@emotion/styled"
+
 function App() {
   return (
-    <></>
+    <button>hi</button>
   )
 }
+
+const Wrapper = styled.div`
+  background-color: ${(({theme})=>theme.colors.gray300)};
+`;
 
 export default App
