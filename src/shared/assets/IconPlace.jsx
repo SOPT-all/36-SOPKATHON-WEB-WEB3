1c379863683169d8dcdcbf66fe2b9c0f2881a67c
jogpfls <joerin4177@gmail.com>
2025-05-18 05:54:22 +0900

    feat: 홈 헤더 구현

---
+++ src/shared/assets/IconPlace.jsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+const SvgIconPlace = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 12 16" {...props}>
+    <path
+      fill="#4B5563"
+      d="M6 8c-.825 0-1.5-.675-1.5-1.5S5.175 5 6 5s1.5.675 1.5 1.5S6.825 8 6 8M6 .5C2.85.5 0 2.915 0 6.65c0 2.49 2.003 5.438 6 8.85q5.999-5.116 6-8.85C12 2.915 9.15.5 6 .5"
+    />
+  </svg>
+);
+export default SvgIconPlace;
--- src/shared/components/Layout/Header.jsx
+++ src/shared/components/Layout/Header.jsx
@@ -1,7 +1,10 @@
-import { useLocation, Link } from 'react-router-dom';
+import { useLocation, Link, useNavigate } from 'react-router-dom';
 import styled from '@emotion/styled';
 
 import IconBack from '@assets/IconBack';
+import IconPlace from '@assets/IconPlace';
+import IconDropDown from '@assets/IconDropDown';
+import routePath from '../../routes/routePath';
 
 export default function Header() {
   const pathname = useLocation().pathname;
@@ -9,9 +12,25 @@ export default function Header() {
   const isHome = !pathname.includes('search') && !pathname.includes('detail');
   const title = pathname.includes('search') ? '출발지 선택' : '상세 페이지';
 
+  const navigate = useNavigate();
+
   return (
     <>
-      {!isHome && (
+      {isHome ? (
+        <HeaderAllWrapper>
+          <HomeHeaderWrapper>
+            <ImgWrapper>
+              <IconPlace />
+            </ImgWrapper>
+            <StationTextWrapper onClick={() => navigate(routePath.SEARCH)}>
+              <StationText>출발지를 선택해주세요</StationText>
+              <ImgDropDouwnWrapper>
+                <IconDropDown/>
+              </ImgDropDouwnWrapper>
+            </StationTextWrapper>
+          </HomeHeaderWrapper>
+        </HeaderAllWrapper>
+      ) : (
         <HeaderWrapper>
           <IconLink to="/">
             <IconBack />
@@ -40,4 +59,44 @@ const HeaderTitle = styled.p`
   left: 50%;
   transform: translateX(-50%);
   ${({ theme }) => theme.font.pre_title_semi_18}
+`;
+
+const HeaderAllWrapper = styled.div`
+  height: 6rem;
+`;
+
+const HomeHeaderWrapper = styled.div`
+  position: fixed;
+  width: 100%;
+  background-color: white;
+  padding: 0 0.8rem;
+  height: 6rem;
+  display: flex;
+  align-items: center;
+  gap: 0.8rem;
+`;
+
+const ImgWrapper = styled.div`
+  width: 1.5rem;
+  height: 2rem;
+  margin-left: 0.8rem;
+  margin-bottom: 0.2rem;
+`;
+
+const StationTextWrapper = styled.div`
+  display: flex;
+  cursor: pointer;
+`;
+
+const StationText = styled.h1`
+  ${({ theme }) => theme.font.pre_subtitle_semi_16};
+  color: ${({theme})=>theme.colors.gray800};
+  margin-top: 0.4rem;
+`;
+
+const ImgDropDouwnWrapper = styled.div`
+  width: 1.2rem;
+  height: 2rem;
+  margin-top: 1rem;
+  margin-left: 0.8rem;
 `;
