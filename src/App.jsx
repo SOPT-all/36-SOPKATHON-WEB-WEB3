9346dcbda48cd8408b1805ddd5e0e62178670dd6
봄 <saebom@Saebomui-MacBookPro.local>
2025-05-17 23:35:04 +0900

    init: Tanstack Query 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
+import { QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import pageRoutes from "@routes/pageRoutes";
-
+import { queryClient } from "@utils/query-client";
 const router = createBrowserRouter(pageRoutes);
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-    </>
+    </QueryClientProvider>
   );
 }
 
--- src/shared/routes/pageRoutes.js
+++
@@ -1,5 +0,0 @@
-import routePath from "@routes/routePath";
-
-const pageRoutes = [{ path: routePath.HOME, element: <Home /> }];
-
-export default pageRoutes;
