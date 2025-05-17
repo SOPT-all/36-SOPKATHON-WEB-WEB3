d329ec74477cd5a6fd0907a66fa78424db545fa8
jstar_00 <wltjd6300@naver.com>
2025-05-18 07:28:35 +0900

    feat: 지하철 리스트 api 연동

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { ThemeProvider } from '@emotion/react'
-import { theme } from './shared/styles/theme.js'
-import GlobalStyle from './shared/styles/globalStyle.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { ThemeProvider } from '@emotion/react';
+import { theme } from './shared/styles/theme.js';
+import GlobalStyle from './shared/styles/globalStyle.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
-      <GlobalStyle/>
+      <GlobalStyle />
       <App />
     </ThemeProvider>
   </StrictMode>,
-)
+);
--- src/pages/Search/SearchPage.jsx
+++ src/pages/Search/SearchPage.jsx
@@ -1,17 +1,46 @@
 import styled from '@emotion/styled';
-
 import StationList from '@pages/Search/components/StationList';
 import Input from '@pages/Search/components/Input';
 import { useState } from 'react';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import apiClient from '../../shared/apis/apiClient';
+import { API } from '../../shared/apis/apis';
 
 export default function SearchPage() {
   const [searchValue, setSearchValue] = useState('');
-  const stations = ['강남', '서울역', '홍대입구', '잠실', '잠실'];
+
+  const fetchSubwayList = async () => {
+    const response = await apiClient.get('/api/v1/subways');
+
+    if (response.status !== 200) {
+      throw new Error('서버 연결 실패');
+    }
+    console.log(response.data.data);
+    return response.data.data;
+  };
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['subway-list'],
+    queryFn: fetchSubwayList, // fetchSubwayList의 return데이터가 data로 들어감
+  });
+
+  const stations = data?.subways ?? [];
 
   const filteredStations = stations.filter((station) => station.includes(searchValue));
+
+  if (isLoading) return <LoadingText>불러오는 중...</LoadingText>;
+  if (error) return <PageWrapper>에러 발생</PageWrapper>;
+
   return (
     <PageWrapper>
       <Input value={searchValue} onChange={setSearchValue} />
+      {!searchValue && (
+        <StationWrapper>
+          {stations.map((station, idx) => {
+            return <StationList key={idx} station={station} />;
+          })}
+        </StationWrapper>
+      )}
       <StationWrapper>
         {searchValue &&
           filteredStations.map((station, idx) => {
@@ -21,6 +50,11 @@ export default function SearchPage() {
     </PageWrapper>
   );
 }
+
+const LoadingText = styled.p`
+  margin: 16px;
+  ${({ theme }) => theme.font.pre_title_semi_18}
+`;
 
 const PageWrapper = styled.div`
   padding: 0 2.8rem;
