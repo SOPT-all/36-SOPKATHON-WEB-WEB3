486b2871630ebaec260796d1df270eb906770316
jstar_00 <wltjd6300@naver.com>
2025-05-18 05:28:09 +0900

    fix: 불필요한 코드 제거

--- src/shared/routes/pageRoutes.jsx
+++ src/shared/routes/pageRoutes.jsx
@@ -2,7 +2,6 @@ import Home from '@pages/Home';
 import routePath from '@routes/routePath';
 import FarmDetail from '../../pages/farmDetail/page/FarmDetail';
 import Search from '@pages/Search/SearchPage';
-import routePath from '@routes/routePath';
 import Layout from '@components/Layout';
 
 const pageRoutes = [
